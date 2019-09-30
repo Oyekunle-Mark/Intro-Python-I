87557b6d643e4e79d51a38a29ebe3b7042e1bc87
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-09-30 12:46:10 +0100

    Solves the bignum problem ;

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,3 +2,11 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
+
+from math import pow
+
+bignum = 2 ** 65536
+
+print(bignum)
+
+# In JS console  Math.pow(2, 65536) and 2 ** 65536 returns infinity

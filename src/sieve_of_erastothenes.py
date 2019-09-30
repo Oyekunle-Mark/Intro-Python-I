82b683f4c4494795d679610686c6a3d27c197f96
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-09-30 16:36:46 +0100

    Documented the functions

--- src/sieve_of_erastothenes.py
+++ src/sieve_of_erastothenes.py
@@ -19,6 +19,15 @@ def is_prime(num):
 
 # refactor to lambda function
 def remove_multiples(ls, num):
+    """Removes the multiple of a number(second argument) from a list(first argument)
+
+    Arguments:
+        ls {list} -- the list to be filtered
+        num {int} -- the multiple of the number to be removed
+
+    Returns:
+        list -- the filtered list
+    """
     new_ls = []
 
     for item in ls:
@@ -29,6 +38,14 @@ def remove_multiples(ls, num):
 
 
 def sieve_of_eratothenes(max):
+    """The famous Sieve of Eratothenes algorith
+
+    Arguments:
+        max {int} -- the upper limit of the prime numbers
+
+    Returns:
+        list -- list of prime numbers between 2 and max
+    """
     prime_numbers = list(range(2, max))
 
     for num in range(2, max):

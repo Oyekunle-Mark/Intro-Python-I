01198f8ccbb27cf3717b2d967cb7c90c5ab92264
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-09-30 14:46:28 +0100

    Receives arguments from the command line and determines if a number is prime or not

--- src/is_prime.py
+++ src/is_prime.py
@@ -6,11 +6,15 @@ def is_prime(num):
         num {int} -- number to be determined if prime
     """
     if num > 1:
-        for n in range(2, num // 2):
+        for n in range(2, (num // 2) + 1):
             if num % n == 0:
                 print(f"{num} is not a prime number")
                 break
         else:
             print(f"{num} is a prime number")
     else:
-        print(f"{num} is a prime number")
+        print(f"{num} is not a prime number")
+
+
+user_input = input("Enter a number: ")
+is_prime(int(user_input))
